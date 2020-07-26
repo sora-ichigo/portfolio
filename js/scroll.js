8f86d7de736616d591782906fe2a64fbac8f05e2
EC2 Default User <ec2-user@ip-172-31-53-37.ec2.internal>
2020-07-26 09:30:02 +0000

    スクロールイベントを設定

---
+++ js/scroll.js
@@ -0,0 +1,51 @@
+$(function() {
+    var pagetop = $('.scroll_button');
+    var skip_top = $('.skip_top');
+    var skip_about = $('.skip_about');
+    var skip_skill = $('.skip_skill');
+    var skip_works = $('.skip_works');
+    var skip_contact = $('.skip_contact');
+
+    // ボタン非表示
+    pagetop.hide();
+
+    // 100px スクロールしたらボタン表示
+    $(window).scroll(function() {
+        if ($(this).scrollTop() > 100) {
+            pagetop.fadeIn();
+        }
+        else {
+            pagetop.fadeOut();
+        }
+    });
+
+    pagetop.click(function() {
+        $('body, html').animate({ scrollTop: 0 }, 500, "swing");
+        return false;
+    });
+    skip_top.click(function() {
+        $('body, html').animate({ scrollTop: 0 }, 500, "swing");
+        return false;
+    });
+    skip_about.click(function() {
+        var about = $(".about").offset().top;
+        $('body, html').animate({ scrollTop: about-50 }, 500, "swing");
+        return false;
+    });
+    skip_skill.click(function() {
+        var skill = $(".skill").offset().top;
+        $('body, html').animate({ scrollTop: skill-50 }, 500, "swing");
+        return false;
+    });
+    skip_works.click(function() {
+        var works = $(".works").offset().top;
+        $('body, html').animate({ scrollTop: works-50 }, 500, "swing");
+        return false;
+    });
+    skip_contact.click(function() {
+        var contact = $(".contact").offset().top;
+        $('body, html').animate({ scrollTop: contact-50 }, 500, "swing");
+        return false;
+    });
+
+});
