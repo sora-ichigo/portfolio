29bc5bd244815998fec723b8abc0b83bcee409aa
EC2 Default User <ec2-user@ip-172-31-53-37.ec2.internal>
2020-07-27 13:28:33 +0000

    ヒーローイメージの高さを変更

--- js/scroll.js
+++ js/scroll.js
@@ -19,6 +19,16 @@ $(function() {
         }
     });
 
+    $(window).scroll(function() {
+        var about = $(".about").offset().top;
+        if ($(this).scrollTop() > about-70 ) {
+            $(".spinner").css("background-color","#000");
+        }
+        else {
+            $(".spinner").css("background-color","#fff");
+        }
+    });
+
     pagetop.click(function() {
         $('body, html').animate({ scrollTop: 0 }, 500, "swing");
         return false;
@@ -29,22 +39,22 @@ $(function() {
     });
     skip_about.click(function() {
         var about = $(".about").offset().top;
-        $('body, html').animate({ scrollTop: about-50 }, 500, "swing");
+        $('body, html').animate({ scrollTop: about - 50 }, 500, "swing");
         return false;
     });
     skip_skill.click(function() {
         var skill = $(".skill").offset().top;
-        $('body, html').animate({ scrollTop: skill-50 }, 500, "swing");
+        $('body, html').animate({ scrollTop: skill - 50 }, 500, "swing");
         return false;
     });
     skip_works.click(function() {
         var works = $(".works").offset().top;
-        $('body, html').animate({ scrollTop: works-50 }, 500, "swing");
+        $('body, html').animate({ scrollTop: works - 50 }, 500, "swing");
         return false;
     });
     skip_contact.click(function() {
         var contact = $(".contact").offset().top;
-        $('body, html').animate({ scrollTop: contact-50 }, 500, "swing");
+        $('body, html').animate({ scrollTop: contact - 50 }, 500, "swing");
         return false;
     });
 
